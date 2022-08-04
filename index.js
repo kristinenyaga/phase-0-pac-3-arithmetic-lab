01cfdbee5a07f8881a8d2d8a36bec1233c0e0d36
kristinenyaga <kristine.nyaga@student.moringaschool.com>
2022-08-04 04:36:56 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,38 @@
+function add(a,b){
+    return a+b
+
+}
+add(1,2)
+function subtract (a,b){
+    return a-b
+}
+subtract(8,4)
+function multiply(a,b){
+    return a*b
+}
+multiply(3,4)
+function divide(a,b){
+    return a/b
+}
+divide(10,5)
+let n = 5
+
+function increment(n){
+    n++
+    return n
+}
+function decrement(n){
+    n--
+    return n
+}
+increment()
+decrement()
+n=165
+function makeInt(n){
+    return parseInt(n,10)
+}
+function preserveDecimal(n){
+    return parseFloat(n,10)
+}
+makeInt()
+preserveDecimal()
